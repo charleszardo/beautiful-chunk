c07381ec03cf1fdd1f65feb24f7532b42910db8d
Dave Charnuska <davecharnuska@gmail.com>
2025-01-18 14:08:15 -0500

    Add App.js component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="scroll-container">
+        <div className="scroll-text">
+          you are a beautiful chunk
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
